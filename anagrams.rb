b28d40b4b939b99373ed3d4109bf1970b2b45d7c
jjparseghian & Lucas Santos <pair+jjparseghian.lucasrsantos1@devbootcamp.com>
2015-02-02 17:31:16 -0500

    add dictionary and anagrams solution

--- anagrams.rb
+++ anagrams.rb
@@ -1,11 +1,36 @@
 require 'sinatra'
 require 'json'
 
+require_relative 'dict'
+
+# dict = open('dict.js')
+
 get '/:word' do |word|
   content_type :json
   # TODO: Return anagrams.
-  [].to_json
+
+
+def is_anagram?(word1, word2)
+  canonical(word1) == canonical(word2)
 end
+
+def canonical(word)
+  word.downcase.split("").sort
+end
+
+def anagrams_for(word1, words)
+  anagrams_found = []
+  words.each do |word|
+    anagrams_found.push(word) if is_anagram?(word1, word)
+  end
+  return anagrams_found
+end
+
+  anagrams = anagrams_for(word, WORDS)
+
+  anagrams.to_json
+end
+
 
 get '/' do
   send_file 'index.html'
